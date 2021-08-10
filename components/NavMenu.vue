15934d2b6869db17b45d0621f0067da005f155e5
Patryk <wacool00@gmail.com>
2021-08-10 20:58:07 +0200

    feat(layout): created layout

---
+++ components/NavMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-list>
+    <v-row no-gutters :class="{ 'flex-column': isFlexColumn }">
+      <v-col v-for="(item, i) in items" :key="i" cols="auto">
+        <v-list-item :to="item.to" nuxt exact>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.name" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-col>
+    </v-row>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: 'NavMenu',
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    isFlexColumn: {
+      type: Boolean,
+      default: true,
+    },
+  },
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -6,26 +6,22 @@ export default {
     titleTemplate: '%s - recruitment_cm',
     title: 'recruitment_cm',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -41,14 +37,13 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -57,13 +52,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
